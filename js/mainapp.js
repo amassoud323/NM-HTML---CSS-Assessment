dff4471e1f322d18ec274f15ee881306a3ce3ba6
amassoud323 <166847845+amassoud323@users.noreply.github.com>
2025-09-29 15:33:15 +0100

    header done and bugs fixed

--- js/mainapp.js
+++ js/mainapp.js
@@ -31,16 +31,66 @@ var main = document.getElementById("main");
 hamburger.onclick = function() {
   main.classList.toggle("active");
   sidebar.classList.toggle("active");
+  hamburger.classList.toggle("active");
 }
 
 // == BANNER OWL CAROUSEL ==
-// $(document).ready(function(){
-//   $('.owl-carousel').owlCarousel();
-// });
-$('.owl-carousel').owlCarousel({
+
+// base carousel function
+var owl = $(".owl-carousel");
+
+owl.owlCarousel({
   items: 1,
   loop: true,
   dots: true,
   autoplay: true,
-  autoplayTimeout: 5000
+  autoplayTimeout: 5000,
+  autoplayHoverPause: false
+});
+
+// function to pause autoplay if user is manually scrolling through slides
+var resumeTimer;
+
+// stop the autoplay timer when user is manually dragging through slides or using dots to navigate slides
+owl.on("translate.owl.carousel drag.owl.carousel", function() {
+  owl.trigger("stop.owl.autoplay");
+  clearTimeout(resumeTimer);
+});
+
+// restart autoplay when user stops manually navigating for 1 second
+owl.on("translated.owl.carousel dragged.owl.carousel", function() {
+  clearTimeout(resumeTimer);
+  resumeTimer = setTimeout(function() {
+    owl.trigger("play.owl.autoplay", [5000]);
+  }, 1000);
+});
+
+// == HEADER SEARCHBAR ANIMATION == 
+$("#desktopSearchBtn").on("click", function() {
+  // make sure function is only active on medium screen sizes
+  if (window.matchMedia("(min-width: 992px) and (max-width: 1260px)").matches) {
+    var $buttons = $(".header__button");
+    var $search = $("#desktopSearch");
+
+    if ($search.css("display") === "none") {
+      $buttons.fadeOut(200, function() {
+        $search.fadeIn(200, function() {
+          $search.css("display", "inline-block");
+          $search.focus();
+        });
+      });
+    } else {
+      $search.fadeOut(200, function() {
+        $buttons.fadeIn(200);
+      });
+    }
+  }
+});
+
+// reset css properties if screen size is outside desired range
+$(window).on("resize", function() {
+  if (!window.matchMedia("(min-width: 992px) and (max-width: 1260px)").matches) {
+    $(".header__button").css("display", "");
+    $("#desktopSearch").css("display", "");
+  }
 });
